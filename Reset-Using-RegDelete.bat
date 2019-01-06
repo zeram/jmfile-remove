73e40489fd4003565af548595ce3aa71599404ca
Zeram <zeram3@gmail.com>
2019-01-06 16:15:00 -0400

    Create new file Reset-Using-RegDelete.bat
    Create new file Reset-Using-RegDelete.bat

---
+++ Reset-Using-RegDelete.bat
@@ -0,0 +1,9 @@
+
+:: https://www.reddit.com/r/SoftwareTrialHaters/comments/6im3bg/tutorial_how_to_reset_trial_of_camtasia_studio/
+:: https://www.youtube.com/watch?v=eS6oeGaNLH0
+:: https://superuser.com/questions/380156/delete-registry-key-or-value-via-a-cmd-script
+reg delete "HKLM\SOFTWARE\Classes\dromjmfile.18" /f
+reg delete "HKLM\SOFTWARE\Classes\dromjmfile.17" /f
+reg delete "HKLM\SOFTWARE\Classes\dromjmfile.16" /f
+reg delete "HKLM\SOFTWARE\Classes\dromjmfile.15" /f
+reg delete "HKLM\SOFTWARE\Classes\dromjmfile" /f 
